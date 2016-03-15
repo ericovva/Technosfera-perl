da613219f998c848e2e9b0aff1a71521f9a1e61f
gmoryes <gmoryes@gmail.com>
2016-03-15 03:35:04 +0300

    Added files via upload

--- homeworks/music_library/bin/music_library.pl
+++ homeworks/music_library/bin/music_library.pl
@@ -1,7 +1,27 @@
 #!/usr/bin/env perl
-
+package bin::music_library;
 use strict;
 use warnings;
+use Data::Dumper;
+use Getopt::Long;
+use lib '/home/gmoryes/Technosfera-perl/homeworks/music_library/lib/';
+ 
+use Local::Parse qw(parse $SORT);
+use Local::Sort qw(sort_hash);
+use Local::PrintTable qw(print_table);
 
-while (<>) {
+my %list = %{parse()};
+my @order = ();
+for (my $i = 0; $i < scalar(keys %list); $i++) {
+	push(@order, $i);
 }
+if ($SORT) {
+	@order = @{sort_hash(\%list, \@order)};
+}
+print_table(\%list, \@order);
+
+
+
+
+
+

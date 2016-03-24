4d7aa75a62eab8582305ea402f3b68701fcefb68
gmoryes <gmoryes@gmail.com>
2016-03-24 15:41:57 +0300

    Added files via upload

--- homeworks/json-parser/lib/Local/JSONParser.pm
+++ homeworks/json-parser/lib/Local/JSONParser.pm
@@ -1,8 +1,10 @@
 package Local::JSONParser;
 
+use utf8;
 use JSON::XS;
 use strict;
 use Unicode::Escape 'unescape';
+use Encode;
 use warnings;
 use base qw(Exporter);
 our @EXPORT_OK = qw( parse_json );
@@ -12,6 +14,10 @@ use DDP;
 sub encodeToText {
 	#преобразуем все спец символы в действительно специальные
 	my $string = shift();
+	if ($string eq "") {
+		return $string;
+	}
+	$string = unescape($string);
 	$string =~ s/\\b/\b/g;
 	$string =~ s/\\f/\f/g;
 	$string =~ s/\\n/\n/g;
@@ -19,42 +25,76 @@ sub encodeToText {
 	$string =~ s/\\t/\t/g;
 	$string =~ s/\\\//\//g;
 	$string =~ s/\\"/"/g;
-	#$string =~ s/\\//g;
+	$string =~ s/\\\\/\\/g;
 	
-	return unescape($string);
+	return decode('utf8', $string);
 }
 
+sub numBracket {
+	my @id = ();
+	my $cnt = 1;
+	my $source = shift;
+	my @arr = split(//, $source);
+	foreach my $i (@arr) {
+		if ($i eq '[' or $i eq '{') {
+			$i = $cnt.$i;
+			push(@id, $cnt++);
+		} elsif ($i eq ']' or $i eq '}') {
+			$i = $i.pop(@id);
+		}
+	}
+	return join('', @arr);
+}
+my $idBracket = 0;
 sub parse_json {
+	#print "new run \n";
 	my $source = shift;
 	$source =~ s/\n//gm;
+	if (!$idBracket) {
+		$source = numBracket($source);
+		$idBracket = 1;
+	}
+	#print "source: $source \n";
 	my ($firstChar) = ($source =~ /([\[|\{])/);
-	
 	#Смотрим на "первый" символ строки, это либо { либо [
 	#В зависимости от него мы начинаем обрабатывать выражение (как массив или как объект)
-	if ($firstChar eq "[") {
+	if ($firstChar eq '[') {
 		#array
 		#удалим окаймляющие скобки, и добавим к концу (,) что бы выражение имело вид:
 		# elem1, elem2, elem3, ... , elemN, (так будет удобнее разбивать по элементам)
-		$source =~ s/([\[|\{])//;
-		$source =~ s/(.*)([\]|\}])/$1/;
-		$source .= ",";
+		
+		$source =~ s/(\d+\[)//;
+		$source =~ s/(.*)(\]\d+)/$1/;
+		#print "source: $source \n";
+		if (!$2) {
+			die "Bad balance";
+		}
 		my @result;
-		while ($source =~ m{
+		my $copySource = $source;
+		$copySource =~ s/\s*//gm;
+		if (length($copySource) == 0) {
+			return \@result;
+		}
+		$source .= ",";
+		
+		#print "1: $source \n";
+		while ($source =~ s{
 			(
-				#Шаблон для string
-				("(?:[^"\\]|\\(?:["\\\/bfnrt]|(?:u\d{4})))+")\s*,
-				|
-				#Шаблон для number
-				((?:\-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE](?:\+|\-)?\d+)?))\s*,
-				|
 				#Шаблон для объекта
-				(\{.*\})\s*,
+				(?<object>(\d+)\{.*\}\3)\s*,
 				|
 				#Шаблон для массива
-				(\[.*\])\s*,
+				(?<array>(\d+)\[.*\]\5)\s*,
+				|
+				#Шаблон для string
+				(?<string>"(?:[^"\\]|\\(?:["\\\/bfnrt]|(?:u\d{4})))*")\s*,
+				|
+				#Шаблон для number
+				(?<number>(?:\-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE](?:\+|\-)?\d+)?))\s*,
 			)
-			}gxm) {
-			my ($string, $number, $object, $array) = ($2, $3, $4, $5);
+			}{}xm) {
+			my ($string, $number, $object, $array) = ($+{string}, $+{number}, $+{object}, $+{array});
+			#print $string."\n";
 			#ищем отдельные элементы массива, если они "элементарные", то сразу добавляем
 			#иначе запускаемся рекурсивно дальше
 			if ($string) {
@@ -64,69 +104,96 @@ sub parse_json {
 				#"превращаем" спец символы в действительно спецсимволы
 				$string = encodeToText($string);
 				push(@result, $string);
-			} elsif ($number) {
-				push(@result, $number);
 			} elsif ($object) {
+				#$object = "0{".$object."}0";
 				push(@result, parse_json($object));
 			} elsif ($array) {
+				#$array = "0[".$array."]0";
 				push(@result, parse_json($array));
+			} elsif ($number or $number == 0) {
+				push(@result, $number);
 			}
 		}
+		$source =~ s/\s*//g;
+		if (length($source) != 0) {
+			die "Wrong syntax in array";
+		}
 		return \@result;
-	} else {
+	} elsif ($firstChar eq '{') {
 		# Делаем все тоже самое, как и с массивом, только теперь элементы имеют
 		# вид string: value
 		#object
-		$source =~ s/([\[|\{])//;
-		$source =~ s/(.*)([\]|\}])/$1/;
-		$source .= ",";
+		
+		$source =~ s/(\d+\{)//;
+		$source =~ s/(.*)(\}\d+)/$1/;
+		if (!$2) {
+			die "Bad balance";
+		}
 		my %result;
-		while ($source =~ m{
+		my $copySource = $source;    
+		$copySource =~ s/\s*//gm;
+		if (length($copySource) == 0) {
+			return \%result;
+		}
+		$source .= ",";
+		#print "1: $source \n";
+		while ($source =~ s{
 			#Шаблон для string (key)
-			("(?:[^"\\]|\\(?:["\\\/bfnrt]|(?:u\d{4})))+")
+			(?<path>"(?:[^"\\]|\\(?:["\\\/bfnrt]|(?:u\d{4})))+")
 			\s*\:\s*
 			(
 				(?:
+					#Шаблон для объекта
+					(?<object>(\d+)\{.*\}\4)\s*
+					|
+					#Шаблон для массива
+					(?<array>(\d+)\[.*\]\6)\s*
+					|
 					#Шаблон для string
-					("(?:[^"\\]|\\(?:["\\\/bfnrt]|(?:u\d{4})))+")
+					(?<string>"(?:[^"\\]|\\(?:["\\\/bfnrt]|(?:u\d{4})))*")\s*
 					|
 					#Шаблон для number
-					((?:\-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE](?:\+|\-)?\d+)?))
-					|
-					#Шаблон для object
-					(\{.*\})
-					|
-					#Шаблон для array
-					(\[.*\])
+					(?<number>(?:\-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE](?:\+|\-)?\d+)?))\s*
 				)
 			)\s*,
-			}xgm) {
-			my ($path, $string, $number, $object, $array) = ($1, $3, $4, $5, $6);
+			}{}xm) {
+				
+			#print "source: $source found: $1 : $2 \n";
+			my ($path, $string, $number, $object, $array) = ($+{path}, $+{string}, $+{number}, $+{object}, $+{array});
 			$path =~ s/^"//;
 			$path =~ s/"$//;
 			$path = encodeToText($path);
-			print "string: $string \n";
 			if ($string) {
 				$string =~ s/^"//;
 				$string =~ s/"$//;
 				$string = encodeToText($string);
 				$result{$path} = $string;
-			} elsif ($number) {
-				$result{$path} = $number;
 			} elsif ($object) {
 				$result{$path} = parse_json($object);
 			} elsif ($array) {
 				$result{$path} = parse_json($array);
+			} elsif ($number or $number == 0) {
+				$result{$path} = $number;
 			}
 		}
+		$source =~ s/\s*//g;
+		if (length($source) != 0) {
+			die "Wrong syntax in object";
+		}
 		return \%result;
+	} else {
+		die "Wrong syntax";
 	}
 	
 	
 	#return JSON::XS->new->utf8->decode($source);
 	return {};
 }
-#p(parse_json('[1,2,"\u0451"]'));
+#p(parse_json('['));
+#p(parse_json('[["31a\u0451sd", 123, [1,[[2]],3]]]'));
+#p(parse_json('{ "key1": "string value", "key2": -3.1415, "key3": ["nested array"], "key4": { "nested": "object" } }'));
+#p(parse_json('[{"key":"1223\t\u0451"}]'));
+#p(parse_json(q/[{ "a":[ "\t\u0451\",","\"," ] }]/));
 #p(parse_json('{ "key1":"string \u0451 \n value","key2":-3.1415,"key3": ["nested array"],"key4":{"nested":"object"}}'));
 #p(parse_json('{"key1":"value"}'));
 1;

c85d00a74cecf663d3f409317a036934a9453613
gmoryes <gmoryes@gmail.com>
2016-05-09 23:36:55 +0300

    how to request xmlrpc

---
+++ homeworks/website2/site2/bin/test.pl
@@ -0,0 +1,16 @@
+use XML::RPC::Fast;
+use XML::RPC::UA::LWP;
+use MIME::Base64;
+use DDP;
+my $uri = "http://localhost:3000/rpc";
+my $rpc = XML::RPC::Fast->new(
+	$uri,
+	ua => XML::RPC::UA::LWP->new(
+		ua      => 'YourApp/0.1',
+		timeout => 3,
+	),
+);
+$rpc->{"ua"}->{"lwp"}->default_header("Authorization" => "Basic ".encode_base64("root:25650294"));
+my $res = $rpc->call("calc.evaluate", "123 + 7 ^ 2 - (3 / 123 )");
+#xml ответ в $rpc->{"xml_in"}
+p $res;
